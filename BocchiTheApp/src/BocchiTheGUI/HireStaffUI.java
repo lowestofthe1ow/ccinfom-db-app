902ce6f72175df808eefbb219228e094358bb25a
Red <148644909+Reddayo@users.noreply.github.com>
2024-11-07 22:36:01 +0800

    made GUI extend JFrame and HireStaffUI extend JPanel.

--- BocchiTheApp/src/BocchiTheGUI/HireStaffUI.java
+++ BocchiTheApp/src/BocchiTheGUI/HireStaffUI.java
@@ -8,9 +8,9 @@ import javax.swing.JButton;
 
 import java.awt.*;
 
-public class HireStaffUI {
+public class HireStaffUI extends JPanel {
     //Hire staff panel
-    private JPanel hireStaffPanel;
+ 
 
     //Hire staff inputs
     private JTextField staffFirstNameTf, staffLastNameTf, staffContactInfoTf;
@@ -20,8 +20,8 @@ public class HireStaffUI {
     private JButton hireStaffConfirmBtn;
 
     public HireStaffUI() {
-        this.hireStaffPanel = new JPanel();
-        this.hireStaffPanel.setLayout((LayoutManager) new BoxLayout(this.hireStaffPanel, BoxLayout.Y_AXIS));
+        
+        this.setLayout((LayoutManager) new BoxLayout(this, BoxLayout.Y_AXIS));
 
         //Set up panels
         JPanel staffFirstNamePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
@@ -34,49 +34,69 @@ public class HireStaffUI {
         staffLastNamePanel.add(new JLabel("Last name: "));
         this.staffLastNameTf = new JTextField();
         this.staffLastNameTf.setColumns(15);
-        staffLastNamePanel.add(this.staffFirstNameTf);
+        staffLastNamePanel.add(this.staffLastNameTf);
 
         JPanel staffContactInfoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         staffContactInfoPanel.add(new JLabel("Contact no.: "));
         this.staffContactInfoTf = new JTextField();
         this.staffContactInfoTf.setColumns(15);
-        staffContactInfoPanel.add(this.staffFirstNameTf);
+        staffContactInfoPanel.add(this.staffContactInfoTf);
 
         JPanel staffPositionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         staffPositionPanel.add(new JLabel("Staff position: "));
         this.staffPositionTf = new JTextField();
         this.staffPositionTf.setColumns(15);
-        staffPositionPanel.add(this.staffFirstNameTf);
+        staffPositionPanel.add(this.staffPositionTf);
 
         JPanel staffSalaryPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         staffSalaryPanel.add(new JLabel("Staff salary: "));
         this.staffSalaryTf = new JTextField();
         this.staffSalaryTf.setColumns(15);
-        staffSalaryPanel.add(this.staffFirstNameTf);
+        staffSalaryPanel.add(this.staffSalaryTf);
 
         JPanel staffStartDatePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         staffStartDatePanel.add(new JLabel("Start date: "));
         this.staffStartDateTf = new JTextField();
         this.staffStartDateTf.setColumns(15);
-        staffStartDatePanel.add(this.staffFirstNameTf);
+        staffStartDatePanel.add(this.staffStartDateTf);
 
         JPanel staffEndDatePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         staffEndDatePanel.add(new JLabel("End date: "));
         this.staffEndDateTf = new JTextField();
         this.staffEndDateTf.setColumns(15);
-        staffEndDatePanel.add(this.staffFirstNameTf);
+        staffEndDatePanel.add(this.staffEndDateTf);
 
         this.hireStaffConfirmBtn = new JButton("Confirm");
 
 
         //Add panels into main hire staff panel
-        this.hireStaffPanel.add(staffFirstNamePanel);
-        this.hireStaffPanel.add(staffLastNamePanel);
-        this.hireStaffPanel.add(staffContactInfoPanel);
-        this.hireStaffPanel.add(staffPositionPanel);
-        this.hireStaffPanel.add(staffSalaryPanel);
-        this.hireStaffPanel.add(staffStartDatePanel);
-        this.hireStaffPanel.add(staffEndDatePanel);
-        this.hireStaffPanel.add(this.hireStaffConfirmBtn);
+        this.add(staffFirstNamePanel);
+        this.add(staffLastNamePanel);
+        this.add(staffContactInfoPanel);
+        this.add(staffPositionPanel);
+        this.add(staffSalaryPanel);
+        this.add(staffStartDatePanel);
+        this.add(staffEndDatePanel);
+        this.add(this.hireStaffConfirmBtn);
     }
+    
+    public String getFirstName() {
+    	return staffFirstNameTf.getName();
+    }
+    
+    public String getLastName() {
+    	return staffFirstNameTf.getName();
+    }
+    public String getContactNo() {
+    	return staffFirstNameTf.getName();
+    }
+  
+    public String getPositionName() {
+    	return staffFirstNameTf.getName();
+    }
+    public Double getSalary() {
+    	return Double.parseDouble(staffSalaryTf.getText());
+    }
+    
 }
+
