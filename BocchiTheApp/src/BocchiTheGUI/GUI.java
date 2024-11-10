c595a640a72818fc93fee7ec0821e78d4de17956
lowestofthe1ow <1owestoftheow@gmail.com>
2024-11-10 18:50:15 +0800

    Create abstract class for text input UI

--- BocchiTheApp/src/BocchiTheGUI/GUI.java
+++ BocchiTheApp/src/BocchiTheGUI/GUI.java
@@ -1,10 +1,12 @@
 package BocchiTheGUI;
 
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -13,59 +15,51 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.border.EmptyBorder;
-
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
-import java.awt.Font;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 public class GUI extends JFrame {
     // Main components
-    
+
     private JPanel contentPanel;
 
     // Buttons
     private JButton updateStaffPosBtn, rentEquipmentBtn, schedAuditionBtn;
-    private JButton resolveBookingBtn, cancelBookingBtn, recordRevenueBtn, generateReportsBtn; 
+    private JButton resolveBookingBtn, cancelBookingBtn, recordRevenueBtn, generateReportsBtn;
     private JButton acceptAuditionBtn;
     private JButton confirmButton;
     private JButton acceptButton, rejectButton;
-    
-    //Menu Item
-    
+
+    // Menu Item
+
     private JMenuItem hireStaffBtn;
     // The other pages
-    
+
     private HireStaffUI hireStaffUI;
     private TableSelectionUI updateStaffPosUI;
     private TableSelectionUI acceptAuditionUI;
     private TableSelectionUI cancelPerformanceUI;
+
     /*
-   
-    private RentEquipmentUI rentEquipmentUI;
-    private SchedAuditionUI schedAuditionUI;
-    private ResolveBookingUI ResolveBookingUI;
-    private CancelBooking cancelBookingUI;
-    private RecordRevenue recordRevenueUI;
-    private GenerateReportsUI generateReportsUI;
-    */
+     * 
+     * private RentEquipmentUI rentEquipmentUI;
+     * private SchedAuditionUI schedAuditionUI;
+     * private ResolveBookingUI ResolveBookingUI;
+     * private CancelBooking cancelBookingUI;
+     * private RecordRevenue recordRevenueUI;
+     * private GenerateReportsUI generateReportsUI;
+     */
     public void initTableColumnNames() {
-    	this.updateStaffPosUI = new TableSelectionUI(new String[] {"ID", "Staff Name", "Current Position"});
-    	this.acceptAuditionUI = new TableSelectionUI(new String[] {"ID", "Performer Name", "Submission Link"});
-    	this.cancelPerformanceUI = new TableSelectionUI(new String[] {"ID", "Performer", "Date", "Start Time", "End Time"});    	
+        this.updateStaffPosUI = new TableSelectionUI(new String[] { "ID", "Staff Name", "Current Position" });
+        this.acceptAuditionUI = new TableSelectionUI(new String[] { "ID", "Performer Name", "Submission Link" });
+        this.cancelPerformanceUI = new TableSelectionUI(
+                new String[] { "ID", "Performer", "Date", "Start Time", "End Time" });
     }
-    
+
     private JMenuBar createMenuBar() {
         JMenuBar menuBar = new JMenuBar();
-        
+
         // Staff Management
         JMenu staffMenu = new JMenu("Staff");
         hireStaffBtn = new JMenuItem("Hire Staff");
@@ -74,7 +68,7 @@ public class GUI extends JFrame {
         staffMenu.add(new JMenuItem("Remove Staff"));
         staffMenu.add(new JMenuItem("Update Staff"));
         menuBar.add(staffMenu);
-       
+
         // Scheduling & Bookings
         JMenu bookingMenu = new JMenu("Booking");
         bookingMenu.add(new JMenuItem("Add Performer"));
@@ -83,7 +77,7 @@ public class GUI extends JFrame {
         bookingMenu.add(new JMenuItem("Cancel Booking"));
         bookingMenu.add(new JMenuItem("Resolve Booking"));
         menuBar.add(bookingMenu);
-        
+
         // Auditions
         JMenu auditionsMenu = new JMenu("Auditions");
         auditionsMenu.add(new JMenuItem("Accept Audition"));
@@ -95,115 +89,111 @@ public class GUI extends JFrame {
         financialsMenu.add(new JMenuItem("Record Revenue"));
         financialsMenu.add(new JMenuItem("Generate Reports"));
         menuBar.add(financialsMenu);
-        
+
         return menuBar;
     }
 
-    
-    
-    
     public GUI() {
         this.contentPanel = new JPanel();
         hireStaffUI = new HireStaffUI();
         initTableColumnNames();
-        //setUndecorated(true);
-        //Set up confirm button for option pane
+        // setUndecorated(true);
+        // Set up confirm button for option pane
         confirmButton = new JButton();
-    	confirmButton.setText("Confirm");
-    	acceptButton = new JButton();
-     	acceptButton.setText("Accept");
-     	rejectButton = new JButton();
-     	rejectButton.setText("Reject");
-     	
-     	JMenuBar menuBar = createMenuBar();
-     	this.setJMenuBar(menuBar);
-        
-     	ImageIcon gifIcon = new ImageIcon(getClass().getClassLoader().getResource("BocchiTheAssets/bocchi-the-rock-bocchi.gif"));
+        confirmButton.setText("Confirm");
+        acceptButton = new JButton();
+        acceptButton.setText("Accept");
+        rejectButton = new JButton();
+        rejectButton.setText("Reject");
+
+        JMenuBar menuBar = createMenuBar();
+        this.setJMenuBar(menuBar);
+
+        ImageIcon gifIcon = new ImageIcon(
+                getClass().getClassLoader().getResource("BocchiTheAssets/bocchi-the-rock-bocchi.gif"));
         JLabel gifLabel = new JLabel(gifIcon);
-     	
-     	
-        //Set up the frame
+
+        // Set up the frame
         this.setTitle("Bocchi the GUI");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
         this.setSize(960, 600);
-        
-        
+
         this.add(gifLabel);
         this.setVisible(true);
-        
-       
-        
+
         /*
-        this.acceptAuditionBtn = new JButton("Accept Audition");
-        this.acceptAuditionBtn.setActionCommand("accept_audition");
-
-        this.acceptAuditionBtn.setPreferredSize(new Dimension(350, 30));
-        this.acceptAuditionBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-        //Set up the buttons
-        
-
-        this.updateStaffPosBtn = new JButton("Update staff positions");
-        this.updateStaffPosBtn.setActionCommand("add_position");
-        this.updateStaffPosBtn.setPreferredSize(new Dimension(350, 30));
-        this.updateStaffPosBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-
-        this.rentEquipmentBtn = new JButton("Rent out equipment for audition or performance");
-        this.rentEquipmentBtn.setPreferredSize(new Dimension(350, 30));
-        this.rentEquipmentBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-
-        this.schedAuditionBtn = new JButton("Schedule an audition");
-        this.schedAuditionBtn.setPreferredSize(new Dimension(350, 30));
-        this.schedAuditionBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-
-        this.resolveBookingBtn = new JButton("Resolve the status of a booking");
-        this.resolveBookingBtn.setPreferredSize(new Dimension(350, 30));
-        this.resolveBookingBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-
-        this.cancelBookingBtn = new JButton("Cancel an existing booking");
-        this.cancelBookingBtn.setPreferredSize(new Dimension(350, 30));
-        this.cancelBookingBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-
-        this.recordRevenueBtn = new JButton("Record a performance's revenue");
-        this.recordRevenueBtn.setPreferredSize(new Dimension(350, 30));
-        this.recordRevenueBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-        
-        this.generateReportsBtn = new JButton("Generate reports");
-        this.generateReportsBtn.setPreferredSize(new Dimension(350, 30));
-        this.generateReportsBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
-
-
-        //Set up the main button panel
-        JPanel mainBtnPanel = new JPanel(new GridLayout(8, 1, 10, 5));
-        mainBtnPanel.setBorder(new EmptyBorder(120, 30, 120, 30));
-
-        //Add buttons to main button panel
-        mainBtnPanel.add(this.hireStaffBtn);
-        mainBtnPanel.add(this.updateStaffPosBtn);
-        mainBtnPanel.add(this.acceptAuditionBtn);
-      
-        mainBtnPanel.add(this.rentEquipmentBtn);
-        mainBtnPanel.add(this.schedAuditionBtn);
-        mainBtnPanel.add(this.resolveBookingBtn);
-        mainBtnPanel.add(this.cancelBookingBtn);
-        mainBtnPanel.add(this.recordRevenueBtn);
-        mainBtnPanel.add(this.generateReportsBtn);
-        
-        
-     
-        
-        
-        //Add main button panel to frame
-        this.add(mainBtnPanel);
-        this.setVisible(true);
-        */
+         * this.acceptAuditionBtn = new JButton("Accept Audition");
+         * this.acceptAuditionBtn.setActionCommand("accept_audition");
+         * 
+         * this.acceptAuditionBtn.setPreferredSize(new Dimension(350, 30));
+         * this.acceptAuditionBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * //Set up the buttons
+         * 
+         * 
+         * this.updateStaffPosBtn = new JButton("Update staff positions");
+         * this.updateStaffPosBtn.setActionCommand("add_position");
+         * this.updateStaffPosBtn.setPreferredSize(new Dimension(350, 30));
+         * this.updateStaffPosBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * 
+         * this.rentEquipmentBtn = new
+         * JButton("Rent out equipment for audition or performance");
+         * this.rentEquipmentBtn.setPreferredSize(new Dimension(350, 30));
+         * this.rentEquipmentBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * 
+         * this.schedAuditionBtn = new JButton("Schedule an audition");
+         * this.schedAuditionBtn.setPreferredSize(new Dimension(350, 30));
+         * this.schedAuditionBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * 
+         * this.resolveBookingBtn = new JButton("Resolve the status of a booking");
+         * this.resolveBookingBtn.setPreferredSize(new Dimension(350, 30));
+         * this.resolveBookingBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * 
+         * this.cancelBookingBtn = new JButton("Cancel an existing booking");
+         * this.cancelBookingBtn.setPreferredSize(new Dimension(350, 30));
+         * this.cancelBookingBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * 
+         * this.recordRevenueBtn = new JButton("Record a performance's revenue");
+         * this.recordRevenueBtn.setPreferredSize(new Dimension(350, 30));
+         * this.recordRevenueBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * 
+         * this.generateReportsBtn = new JButton("Generate reports");
+         * this.generateReportsBtn.setPreferredSize(new Dimension(350, 30));
+         * this.generateReportsBtn.setFont(new Font("Dialog", Font.PLAIN, 15));
+         * 
+         * 
+         * //Set up the main button panel
+         * JPanel mainBtnPanel = new JPanel(new GridLayout(8, 1, 10, 5));
+         * mainBtnPanel.setBorder(new EmptyBorder(120, 30, 120, 30));
+         * 
+         * //Add buttons to main button panel
+         * mainBtnPanel.add(this.hireStaffBtn);
+         * mainBtnPanel.add(this.updateStaffPosBtn);
+         * mainBtnPanel.add(this.acceptAuditionBtn);
+         * 
+         * mainBtnPanel.add(this.rentEquipmentBtn);
+         * mainBtnPanel.add(this.schedAuditionBtn);
+         * mainBtnPanel.add(this.resolveBookingBtn);
+         * mainBtnPanel.add(this.cancelBookingBtn);
+         * mainBtnPanel.add(this.recordRevenueBtn);
+         * mainBtnPanel.add(this.generateReportsBtn);
+         * 
+         * 
+         * 
+         * 
+         * 
+         * //Add main button panel to frame
+         * this.add(mainBtnPanel);
+         * this.setVisible(true);
+         */
         dialogInit();
-         
+
     }
+
     public void setWindowListener(WindowAdapter windowAdapter) {
-    	this.addWindowListener(windowAdapter);
+        this.addWindowListener(windowAdapter);
     }
-    
+
     public void setBtnAActionListener(ActionListener actionListener) {
         this.hireStaffBtn.addActionListener(actionListener);
     }
@@ -217,105 +207,102 @@ public class GUI extends JFrame {
     }
 
     public void setButtonPanelActionListener(ActionListener listener) {
-    	this.confirmButton.addActionListener(listener);
-    	this.acceptButton.addActionListener(listener);
-    	this.rejectButton.addActionListener(listener);
+        this.confirmButton.addActionListener(listener);
+        this.acceptButton.addActionListener(listener);
+        this.rejectButton.addActionListener(listener);
     }
+
     public HireStaffUI getHireStaff() {
-    	return this.hireStaffUI;
+        return this.hireStaffUI;
     }
+
     public void dialogInit() {
-    	
-    	
-    	dialog.getContentPane().setLayout(new BorderLayout());
-        dialog.setLocationRelativeTo(null); 
-        dialog.setModal(true); 
+
+        dialog.getContentPane().setLayout(new BorderLayout());
+        dialog.setLocationRelativeTo(null);
+        dialog.setModal(true);
         dialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-    	
-    	confirmBtnPanel.add(confirmButton);
-    	confirmBtnPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 10));
-    	acceptRejectBtnPanel.add(acceptButton);
-    	acceptRejectBtnPanel.add(rejectButton);
-    	acceptRejectBtnPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 10));
-    	
-    	dialog.setSize(400, 360);
-    	
-    	acceptButton.setActionCommand("accept_audition");
-    	rejectButton.setActionCommand("reject_audition");
-    	
-    	dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE); 
-    	
+
+        confirmBtnPanel.add(confirmButton);
+        confirmBtnPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 10));
+        acceptRejectBtnPanel.add(acceptButton);
+        acceptRejectBtnPanel.add(rejectButton);
+        acceptRejectBtnPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 10));
+
+        dialog.setSize(400, 360);
+
+        acceptButton.setActionCommand("accept_audition");
+        rejectButton.setActionCommand("reject_audition");
+
+        dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+
         dialog.addWindowListener(new WindowAdapter() {
-             @Override
+            @Override
             public void windowClosing(WindowEvent e) {
-            	
+
                 dialog.setVisible(false);
                 removeText();
-             }
+            }
         });
-    	
-    	confirmButton.addActionListener(e -> {
-    		
-    		    dialog.setVisible(false);
-    		    removeText();
+
+        confirmButton.addActionListener(e -> {
+
+            dialog.setVisible(false);
+            removeText();
         });
-    	
-    	acceptButton.addActionListener(e -> {
-   	     	
-    		
-		    dialog.setVisible(false);
-		    removeText();
-    	});
-    	rejectButton.addActionListener(e -> {
-   	     	
-    		
-		    dialog.setVisible(false);
-		    removeText();
-    	});
+
+        acceptButton.addActionListener(e -> {
+
+            dialog.setVisible(false);
+            removeText();
+        });
+        rejectButton.addActionListener(e -> {
+
+            dialog.setVisible(false);
+            removeText();
+        });
     }
-    
-   
-    
 
     public void removeText() {
-    	hireStaffUI.removeText();
+        hireStaffUI.removeText();
     }
-    
+
     private JDialog dialog = new JDialog();
     private JPanel confirmBtnPanel = new JPanel();
     private JPanel acceptRejectBtnPanel = new JPanel();
-    
+
     public void showDialog(String command) {
-    	System.out.println("Command that is passed in setActionCommand is " + command  + ".");
-    	dialog.getContentPane().removeAll();
-    	 SwingUtilities.invokeLater(new Runnable() {
-    	        public void run() {
-    	            switch(command) {
-    	                case "hire":
-    	                    dialog.getContentPane().add(hireStaffUI, BorderLayout.CENTER); 
-    	                    break;
-    	                case "add_position":
-    	                    dialog.getContentPane().add(updateStaffPosUI, BorderLayout.CENTER); 
-    	                    break;
-    	                case "accept_audition":
-    	                	dialog.getContentPane().add(acceptAuditionUI, BorderLayout.CENTER);
-    	                   
-    	            }
-    	            switch(command) {
-    	            case "accept_audition":
-    	            	dialog.getContentPane().add(acceptRejectBtnPanel, BorderLayout.SOUTH);
-    	            	break;
-    	            default:  dialog.getContentPane().add(confirmBtnPanel, BorderLayout.SOUTH);
-    	            		  confirmButton.setActionCommand(command);
-    	            }
-    	            
-    	            dialog.setVisible(true);
-    	        }
-    	    });
+        System.out.println("Command that is passed in setActionCommand is " + command + ".");
+        dialog.getContentPane().removeAll();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                switch (command) {
+                    case "hire":
+                        dialog.getContentPane().add(hireStaffUI, BorderLayout.CENTER);
+                        break;
+                    case "add_position":
+                        dialog.getContentPane().add(updateStaffPosUI, BorderLayout.CENTER);
+                        break;
+                    case "accept_audition":
+                        dialog.getContentPane().add(acceptAuditionUI, BorderLayout.CENTER);
+
+                }
+                switch (command) {
+                    case "accept_audition":
+                        dialog.getContentPane().add(acceptRejectBtnPanel, BorderLayout.SOUTH);
+                        break;
+                    default:
+                        dialog.getContentPane().add(confirmBtnPanel, BorderLayout.SOUTH);
+                        confirmButton.setActionCommand(command);
+                }
+
+                dialog.setVisible(true);
+            }
+        });
     }
-    
+
     public TableSelectionUI getAccAud() {
-    	return acceptAuditionUI;
+        return acceptAuditionUI;
     }
 
     public void setBtnDActionListener(ActionListener actionListener) {
--- BocchiTheApp/src/BocchiTheGUI/HireStaffUI.java
+++ BocchiTheApp/src/BocchiTheGUI/HireStaffUI.java
@@ -1,110 +1,27 @@
 package BocchiTheGUI;
 
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-
-import java.awt.*;
-
-public class HireStaffUI extends JPanel {
-    //Hire staff panel
- 
-
-    //Hire staff inputs
-    private JTextField staffFirstNameTf, staffLastNameTf, staffContactInfoTf;
-    private JTextField staffPositionTf, staffSalaryTf;
-   //  private JTextField staffStartDateTf, staffEndDateTf;
-
-    //Confirm button
-   // private JButton hireStaffConfirmBtn;
-
+public class HireStaffUI extends TextFieldsUI {
     public HireStaffUI() {
-        
-        this.setLayout((LayoutManager) new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        //Set up panels
-        JPanel staffFirstNamePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        staffFirstNamePanel.add(new JLabel("First name: "));
-        this.staffFirstNameTf = new JTextField();
-        this.staffFirstNameTf.setColumns(15);
-        staffFirstNamePanel.add(this.staffFirstNameTf);
-
-        JPanel staffLastNamePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        staffLastNamePanel.add(new JLabel("Last name: "));
-        this.staffLastNameTf = new JTextField();
-        this.staffLastNameTf.setColumns(15);
-        staffLastNamePanel.add(this.staffLastNameTf);
-
-        JPanel staffContactInfoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        staffContactInfoPanel.add(new JLabel("Contact no.: "));
-        this.staffContactInfoTf = new JTextField();
-        this.staffContactInfoTf.setColumns(15);
-        staffContactInfoPanel.add(this.staffContactInfoTf);
-
-        JPanel staffPositionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        staffPositionPanel.add(new JLabel("Staff position: "));
-        this.staffPositionTf = new JTextField();
-        this.staffPositionTf.setColumns(15);
-        staffPositionPanel.add(this.staffPositionTf);
-
-        JPanel staffSalaryPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        staffSalaryPanel.add(new JLabel("Staff salary: "));
-        this.staffSalaryTf = new JTextField();
-        this.staffSalaryTf.setColumns(15);
-        staffSalaryPanel.add(this.staffSalaryTf);
-/*
-        JPanel staffStartDatePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        staffStartDatePanel.add(new JLabel("Start date: "));
-        this.staffStartDateTf = new JTextField();
-        this.staffStartDateTf.setColumns(15);
-        staffStartDatePanel.add(this.staffStartDateTf);
-
-        JPanel staffEndDatePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        staffEndDatePanel.add(new JLabel("End date: "));
-        this.staffEndDateTf = new JTextField();
-        this.staffEndDateTf.setColumns(15);
-        staffEndDatePanel.add(this.staffEndDateTf);
-*/
-        //this.hireStaffConfirmBtn = new JButton("Confirm");
-
-
-        //Add panels into main hire staff panel
-        this.add(staffFirstNamePanel);
-        this.add(staffLastNamePanel);
-        this.add(staffContactInfoPanel);
-        this.add(staffPositionPanel);
-        this.add(staffSalaryPanel);
-        //this.add(staffStartDatePanel);
-        //this.add(staffEndDatePanel);
-        //this.add(this.hireStaffConfirmBtn);
+        super("First name: ", "Last name: ", "Contact number: ", "Staff position: ", "Salary: ");
     }
-    
+
     public String getFirstName() {
-    	return staffFirstNameTf.getText();
+        return this.formItems.get(0).getText();
     }
-    
+
     public String getLastName() {
-    	return staffLastNameTf.getText();
+        return this.formItems.get(1).getText();
     }
+
     public Long getContactNo() {
-    	return Long.parseLong(staffContactInfoTf.getText());
+        return Long.parseLong(this.formItems.get(2).getText());
     }
-  
+
     public String getPositionName() {
-    	return staffPositionTf.getText();
+        return this.formItems.get(3).getText();
     }
+
     public Double getSalary() {
-    	return Double.parseDouble(staffSalaryTf.getText());
-    }
-    public void removeText() {
-    	staffFirstNameTf.setText("");
-    	staffLastNameTf.setText("");
-    	staffPositionTf.setText("");
-    	staffContactInfoTf.setText("");
-    	staffSalaryTf.setText("");
-    	
+        return Double.parseDouble(this.formItems.get(4).getText());
     }
 }
-
