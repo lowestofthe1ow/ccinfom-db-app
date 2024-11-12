f7a4c46b246194ee6d3526d6b47176e9d10b59bd
lowestofthe1ow <1owestoftheow@gmail.com>
2024-11-12 11:31:16 +0800

    Loop through selection in TableSelectionUI instead of single selection

--- BocchiTheApp/src/BocchiTheGUI/AuditionSelectionUI.java
+++ BocchiTheApp/src/BocchiTheGUI/AuditionSelectionUI.java
@@ -1,29 +1,37 @@
 package BocchiTheGUI;
 
+import java.util.ArrayList;
+
 public class AuditionSelectionUI extends TableSelectionUI {
-	
-	AuditionSelectionUI(){
-		
-		 super("Audition Selection", "ID", "Performer Name", "Submission Link");
-		 super.addButtons("Accept");
-	     super.addButtons("Reject");
-	     setButtonActionCommands("accept_audition", "reject_audition");
+
+	AuditionSelectionUI() {
+
+		super("Audition Selection", "ID", "Performer Name", "Submission Link");
+		super.addButtons("Accept");
+		super.addButtons("Reject");
+		setButtonActionCommands("accept_audition", "reject_audition");
 	}
-	
+
 	@Override
 	public Object[] getSQLParameterInputs() {
-		int selectedRowIndex = table.getSelectedRow();
-	     
-        if (selectedRowIndex == -1) {
-            return null;  
-        }
-        
-        Object[] idObject = {table.getValueAt(selectedRowIndex, 0)}; 
-        if (idObject[0] instanceof Integer) {
-            return idObject;  
-        } else {
-            throw new IllegalArgumentException("Selected row does not have an Integer ID.");
-        }
-		//return null;
-	} 
+		int[] selectedRowIndices = table.getSelectedRows();
+
+		if (selectedRowIndices.length == 0) {
+			return null;
+		}
+
+		ArrayList<Object> retval = new ArrayList<>();
+
+		for (int selectedRowIndex : selectedRowIndices) {
+			Object val = table.getValueAt(selectedRowIndex, 0);
+
+			if (val instanceof Integer) {
+				retval.add(val);
+			} else {
+				throw new IllegalArgumentException("Selected row(s) does not have an Integer ID.");
+			}
+		}
+
+		return retval.toArray();
+	}
 }
