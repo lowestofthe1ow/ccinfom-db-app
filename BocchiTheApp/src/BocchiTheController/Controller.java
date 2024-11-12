59868501771961232cd146035a5209d18cc30ab2
Red <148644909+Reddayo@users.noreply.github.com>
2024-11-12 11:40:44 +0800

    A

--- BocchiTheApp/src/BocchiTheController/Controller.java
+++ BocchiTheApp/src/BocchiTheController/Controller.java
@@ -174,46 +174,6 @@ public class Controller {
     
     	return rows;
     }
-    private void selectStaff() {
-        String selectSql = "SELECT * FROM staff";
-        try (Statement statement = this.connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(selectSql)) {
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String contactNo = resultSet.getString("contact_no");
-
-                System.out.println("ID: " + id +
-                        ", First Name: " + firstName +
-                        ", Last Name: " + lastName +
-                        ", Contact No: " + contactNo);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void selectStaffPosition() {
-        String selectSql = "SELECT * FROM staff_position";
-        try (Statement statement = this.connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(selectSql)) {
-            while (resultSet.next()) {
-                int staffId = resultSet.getInt("staff_id");
-                String positionName = resultSet.getString("staff_position_name");
-                double salary = resultSet.getDouble("staff_salary");
-                Date startDate = resultSet.getDate("start_date");
-                Date endDate = resultSet.getDate("end_date");
-
-                System.out.println("Staff ID: " + staffId +
-                        ", Position: " + positionName +
-                        ", Salary: " + salary +
-                        ", Start Date: " + startDate +
-                        ", End Date: " + (endDate != null ? endDate : "Currently Employed"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+    
+    
 }
